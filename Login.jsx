81c2c0600bf03e10225eeaf47f96045b2160d3b2
Sivasankari-N <153375756+Sivasankari-N@users.noreply.github.com>
2023-12-14 11:39:59 +0530

    Add files via upload

---
+++ Login.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    
+    if (username && password) {
+      alert('Login successful!');
+     
+    } else {
+      alert('Invalid credentials. Please try again.');
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form>
+        <label>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <br />
+        <button type="button" onClick={handleLogin}>
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
